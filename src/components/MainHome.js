740b210bf6747e984f95371cec4838e020ae36e6
leogytis <leogytis@gmail.com>
2024-01-29 09:19:34 +0200

    little fix for local

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -2,75 +2,75 @@ import styled from "styled-components";
 import Tabs from "./Tabs";
 
 const MainHome = () => {
-  return (
-    <Container id="home">
-      <Row>
-        <Header1 bg="#ff5421">Start learning today</Header1>
-        <Header2>Online Courses From Leading Experts</Header2>
-        <Button>Find Course</Button>
-      </Row>
-      <Tabs />
-    </Container>
-  );
+	return (
+		<Container id="home">
+			<Row>
+				<Header1 bg="#ff5421">Start learning today</Header1>
+				<Header2>Online Courses From Leading Experts</Header2>
+				<Button>Find Course</Button>
+			</Row>
+			<Tabs />
+		</Container>
+	);
 };
 
 export default MainHome;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  background-image: url("./images/photos/001.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: auto;
-  background-size: cover;
-  color: #fff;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	background-image: url("./images/photos/001.jpg");
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: auto;
+	background-size: cover;
+	color: #fff;
+	/* "homepage": "https://leogytis.github.io/e-study", */
 `;
 
 const Row = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  text-align: center;
-  padding: 250px 0 200px 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	padding: 250px 0 200px 0;
 `;
 
 const Header1 = styled.div`
-  font-size: 22px;
-  line-height: 30px;
-  font-weight: 700;
-  color: #ff5421;
-  margin-bottom: 22px;
-  text-transform: uppercase;
+	font-size: 22px;
+	line-height: 30px;
+	font-weight: 700;
+	color: #ff5421;
+	margin-bottom: 22px;
+	text-transform: uppercase;
 `;
 
 const Header2 = styled.div`
-  max-width: 750px;
-  font-size: 70px;
-  line-height: 80px;
-  font-weight: 700;
-  margin-bottom: 65px;
+	max-width: 750px;
+	font-size: 70px;
+	line-height: 80px;
+	font-weight: 700;
+	margin-bottom: 65px;
 `;
 
 const Button = styled.button`
-  font-size: 16px;
-  font-weight: 700;
-  text-transform: uppercase;
-  border-radius: 5px;
-  border: none;
-  cursor: pointer;
-  padding: 15px 30px !important;
-  background: #ff5421;
-  color: #fff;
-  &:hover {
-    background: #171f32;
-    opacity: 0.9;
-    transform: scale(0.98);
-  }
-  transition: all 0.5s ease;
+	font-size: 16px;
+	font-weight: 700;
+	text-transform: uppercase;
+	border-radius: 5px;
+	border: none;
+	cursor: pointer;
+	padding: 15px 30px !important;
+	background: #ff5421;
+	color: #fff;
+	&:hover {
+		background: #171f32;
+		opacity: 0.9;
+		transform: scale(0.98);
+	}
+	transition: all 0.5s ease;
 `;
--- src/components/NewsUpdate.js
+++ src/components/NewsUpdate.js
@@ -3,163 +3,162 @@ import Icons from "../utils/Icons";
 import { newsInfo } from "../utils/texts";
 
 const NewsUpdate = () => {
-  return (
-    <Container>
-      <Header1>News Update</Header1>
-      <Header2>Latest News & events</Header2>
-      <Row>
-        {newsInfo.map((news, i) => {
-          return (
-            <News>
-              <Image
-                key={`coursecontainer-${i}`}
-                image={`./images/photos/05${++i}` + `.jpg`}
-                alt={news.header}
-              />
-              <Content>
-                <InfoDetails>
-                  <Detail>
-                    <Icon name={"date"} />
-                    {news.date}
-                  </Detail>
-                  <Detail>
-                    <Icon name={"user"} />
-                    {news.user}
-                  </Detail>
-                </InfoDetails>
-                <ContentHeader>{news.header}</ContentHeader>
-                <Comment>{news.comment} </Comment>
-                <GreyLine />
-                <InfoDetails>
-                  <Detail>
-                    <Icon name={"book"} />
-                    {news.place}
-                  </Detail>
-                  <ReadMore>ReadMore ---</ReadMore>
-                </InfoDetails>
-              </Content>
-            </News>
-          );
-        })}
-      </Row>
-    </Container>
-  );
+	return (
+		<Container>
+			<Header1>News Update</Header1>
+			<Header2>Latest News & events</Header2>
+			<Row>
+				{newsInfo.map((news, i) => {
+					return (
+						<News key={`newscontainer-${i}`}>
+							<Image
+								image={`./images/photos/05${++i}` + `.jpg`}
+								alt={news.header}
+							/>
+							<Content>
+								<InfoDetails>
+									<Detail>
+										<Icon name={"date"} />
+										{news.date}
+									</Detail>
+									<Detail>
+										<Icon name={"user"} />
+										{news.user}
+									</Detail>
+								</InfoDetails>
+								<ContentHeader>{news.header}</ContentHeader>
+								<Comment>{news.comment} </Comment>
+								<GreyLine />
+								<InfoDetails>
+									<Detail>
+										<Icon name={"book"} />
+										{news.place}
+									</Detail>
+									<ReadMore>ReadMore ---</ReadMore>
+								</InfoDetails>
+							</Content>
+						</News>
+					);
+				})}
+			</Row>
+		</Container>
+	);
 };
 
 export default NewsUpdate;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-top: 100px;
-  padding-bottom: 100px;
-  /* background-color: goldenrod; */
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding-top: 100px;
+	padding-bottom: 100px;
+	/* background-color: goldenrod; */
 `;
 
 const Header1 = styled.div`
-  font-size: 18px;
-  line-height: 28px;
-  font-weight: 600;
-  color: #ff5421;
-  text-transform: uppercase;
-  margin-bottom: 10px;
+	font-size: 18px;
+	line-height: 28px;
+	font-weight: 600;
+	color: #ff5421;
+	text-transform: uppercase;
+	margin-bottom: 10px;
 `;
 
 const Header2 = styled.div`
-  font-size: 36px;
-  line-height: 46px;
-  font-weight: 800;
-  color: #101010;
-  margin-bottom: 26px;
+	font-size: 36px;
+	line-height: 46px;
+	font-weight: 800;
+	color: #101010;
+	margin-bottom: 26px;
 `;
 
 const Row = styled.div`
-  max-width: 1270px;
-  margin: auto;
-  display: flex;
-  gap: 30px;
-  padding: 20px;
-  box-shadow: inset 0 -150px 200px -50px rgba(211, 211, 211, 0.4);
+	max-width: 1270px;
+	margin: auto;
+	display: flex;
+	gap: 30px;
+	padding: 20px;
+	box-shadow: inset 0 -150px 200px -50px rgba(211, 211, 211, 0.4);
 `;
 
 const News = styled.div`
-  height: 100%;
-  font-size: 36px;
-  line-height: 46px;
-  font-weight: 800;
-  border-radius: 4px;
-  color: #101010;
-  margin-bottom: 26px;
-  background-color: #fff;
-  border: 1px solid #b9b8b1;
+	height: 100%;
+	font-size: 36px;
+	line-height: 46px;
+	font-weight: 800;
+	border-radius: 4px;
+	color: #101010;
+	margin-bottom: 26px;
+	background-color: #fff;
+	border: 1px solid #b9b8b1;
 `;
 
 const Image = styled.div`
-  height: 250px;
-  background-image: url(${({ image }) => image || "none"});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: auto;
-  background-size: cover;
-  &:hover {
-    cursor: pointer;
-  }
+	height: 250px;
+	background-image: url(${({ image }) => image || "none"});
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: auto;
+	background-size: cover;
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 5px 20px;
-  z-index: 20;
-  overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	padding: 5px 20px;
+	z-index: 20;
+	overflow: hidden;
 `;
 
 const ContentHeader = styled.div`
-  font-size: 16px;
-  font-weight: 700;
-  line-height: 20px;
-  margin: 10px 0 15px 0;
-  color: black;
-  &:hover {
-    cursor: pointer;
-    color: #ff5421;
-  }
-  transition: all 0.3s ease;
+	font-size: 16px;
+	font-weight: 700;
+	line-height: 20px;
+	margin: 10px 0 15px 0;
+	color: black;
+	&:hover {
+		cursor: pointer;
+		color: #ff5421;
+	}
+	transition: all 0.3s ease;
 `;
 
 const Comment = styled.div`
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 12px;
+	font-size: 12px;
+	font-weight: 500;
+	line-height: 12px;
 
-  margin-bottom: 10px;
+	margin-bottom: 10px;
 `;
 
 const GreyLine = styled.div`
-  margin: 30px 10px 0px 10px;
-  border-top: 1px solid #f4f0f0;
+	margin: 30px 10px 0px 10px;
+	border-top: 1px solid #f4f0f0;
 `;
 
 const InfoDetails = styled.div`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const Detail = styled.div`
-  font-size: 10px;
-  font-weight: 500;
+	font-size: 10px;
+	font-weight: 500;
 `;
 
 const Icon = styled(Icons)`
-  font-size: 10px;
-  margin-bottom: -1px;
-  color: #ff5421;
-  margin-right: 8px;
+	font-size: 10px;
+	margin-bottom: -1px;
+	color: #ff5421;
+	margin-right: 8px;
 `;
 
 const ReadMore = styled.div`
-  font-size: 12px;
-  font-weight: 500;
+	font-size: 12px;
+	font-weight: 500;
 `;
